40575c1db66dd0a6fb13f2976877a86d845bde97
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-25 12:07:45 +0530

    Setup for UI Theme, Colors and Shapes done

--- app/src/main/java/com/example/shrine_ecommerce/ui/theme/Color.kt
+++ app/src/main/java/com/example/shrine_ecommerce/ui/theme/Color.kt
@@ -1,11 +1,27 @@
 package com.example.shrine_ecommerce.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material.lightColors
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val ShrinePink10 = Color(0xfffffbfa)
+val ShrinePink50 = Color(0xfffeeae6)
+val ShrinePink100 = Color(0xfffedbd0)
+val ShrinePink300 = Color(0xfffff0ea)
+val ShrinePink500 = Color(0xfffbb8ac)
+val ShrinePink900 = Color(0xff442c2e)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ShrineScrimColor = ShrinePink300
+
+internal val ShrineLightColorPalette = lightColors(
+    primary = ShrinePink100,
+    primaryVariant = ShrinePink500,
+    secondary = ShrinePink50,
+    background = ShrinePink100,
+    surface = ShrinePink10,
+    error = Color(0xffc5032b),
+    onPrimary = ShrinePink900,
+    onSecondary = ShrinePink900,
+    onBackground = ShrinePink900,
+    onSurface = ShrinePink900,
+    onError = ShrinePink10
+)
--- app/src/main/java/com/example/shrine_ecommerce/ui/theme/Theme.kt
+++ app/src/main/java/com/example/shrine_ecommerce/ui/theme/Theme.kt
@@ -1,70 +1,101 @@
 package com.example.shrine_ecommerce.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
+import androidx.compose.material.Text
+import androidx.compose.material.Shapes
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ShrineECommerceTheme(
+fun ShrineComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable() () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+    val colors = if (darkTheme) {
+        // TODO: Add dark colors in the future
+        ShrineLightColorPalette
+    } else {
+        ShrineLightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colors = colors,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
+}
+
+@Preview(name = "Theme test", showBackground = true)
+@Composable
+fun ThemeTest() {
+    Column(
+        Modifier.padding(48.dp),
+    ) {
+        ShrineComposeTheme {
+            Button(onClick = {}) {
+                Text("Button1")
+            }
+            Spacer(Modifier.height(16.dp))
+            Card {
+                Column(
+                    Modifier.padding(16.dp)
+                ) {
+                    Text("This is a card")
+                }
+            }
+        }
+        Spacer(Modifier.height(16.dp))
+        MaterialTheme {
+            Button(onClick = {}) {
+                Text("Button1")
+            }
+            Spacer(Modifier.height(16.dp))
+            Card {
+                Column(
+                    Modifier.padding(16.dp)
+                ) {
+                    Text("This is a card")
+                }
+            }
+        }
+    }
+}
+
+@Preview(name = "Typography test", widthDp = 720, showBackground = true)
+@Composable
+fun TypographyThemeTest() {
+    ShrineComposeTheme {
+        Column {
+            Text(
+                "H1 / Rubik Light",
+                style = MaterialTheme.typography.h1
+            )
+            Text("H2 / Rubik Light",
+                style = MaterialTheme.typography.h2
+            )
+            Text("H3 / Rubik Regular",
+                style = MaterialTheme.typography.h3
+            )
+            Text("Body1 / Rubik Regular",
+                style = MaterialTheme.typography.body1
+            )
+            Text("Button / Rubik Medium".uppercase(),
+                style = MaterialTheme.typography.button
+            )
+            Text("Caption / Rubik Regular",
+                style = MaterialTheme.typography.caption
+            )
+        }
+    }
 }
